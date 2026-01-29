6cb7935601abd3798221043ea3ec701dda09e2cd
csae8092 <p.andorfer@gmail.com>
2026-01-29 15:14:49 +0100

    minor

--- src/routes/datasets/+page.svelte
+++ src/routes/datasets/+page.svelte
@@ -7,11 +7,6 @@
 </svelte:head>
 <div class="p-3 text-center xl:mx-20 2xl:mx-80">
 	<h1 class="p-3 text-5xl font-bold">Datasets</h1>
-	<h2 class="p-2 text-2xl font-medium md:text-3xl">Explore data from { data.count }</h2>
-	<p class="p-1 md:text-xl">
-		The PFP – Prosopographical Research Platform Austria serves as an umbrella project for
-		person-based research at the Austrian Academy of Sciences (OeAW) using methods of Digital
-		Humanities. In this platform, data from prosopographical projects, digital editions and the
-		Austrian Biographical Dictionary are integrated.
-	</p>
+	<h2 class="p-2 text-2xl font-medium md:text-3xl">Explore data from { data.count } Datasets</h2>
+	
 </div>
