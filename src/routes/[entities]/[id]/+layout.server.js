948e12b78f94fd2c7895e6379c121e9af57b9aa4
csae8092 <p.andorfer@gmail.com>
2026-01-30 11:30:40 +0100

    minor [skip ci]

--- src/routes/[entities]/[id]/+layout.server.js
+++ src/routes/[entities]/[id]/+layout.server.js
@@ -6,8 +6,7 @@ export async function load({ fetch, params, url }) {
     let fetch_data_url = `${appConfig.base_url}${params.entities.slice(0, -1)}/${params.id}`
     const res = await fetch(fetch_data_url);
 	const payload = await res.json();
-    
-    console.log(params)
-    console.log(url)
+    // console.log(params)
+    // console.log(url)
     return { fetch_data_url, payload }
 }
